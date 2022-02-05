8ab82a439f6d930caa5799065707b649e5dab188
xowns4817 <xowns4817@naver.com>
2022-02-05 16:47:24 +0900

    testCode disable 처리

--- src/test/java/com/example/file/server/demo/FileServerDemoApplicationTests.java
+++ src/test/java/com/example/file/server/demo/FileServerDemoApplicationTests.java
@@ -42,6 +42,7 @@ class FileServerDemoApplicationTests {
 
     @Test
     @DisplayName("이미지 업로드 API")
+    @Disabled
     void imgUploadTest(){
 
         String roomId = "aaaaaa";
@@ -65,6 +66,7 @@ class FileServerDemoApplicationTests {
 
     @Test
     @DisplayName("이미지 식제 API")
+    @Disabled
     void imgDeleteTest(){
 
         String roomId = "aaaaaa";
