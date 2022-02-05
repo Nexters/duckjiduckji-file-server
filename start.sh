d3acf35e204878626435bd307fd21019ff69d88b
xowns4817 <xowns4817@naver.com>
2022-02-05 17:50:06 +0900

    start.sh 파일 수정

--- start.sh
+++ start.sh
@@ -4,6 +4,12 @@ echo "-----start duckjiduckji file-server--------"
 
 profiles=$1
 
+if [ -z "$profiles" ]
+then
+        echo "check profiles!!"
+        exit 0
+fi
+
 kill -9 `ps -ef | grep duckjiduckji-fileServer* | awk '{print $2}'`
 
 nohup java -jar -Dspring.profiles.active=$profiles *.jar &
